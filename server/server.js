e4331682150fbfcf480a7e43d58849d84dcea0ae
Ganga178 <gangamu718@gmail.com>
2026-02-22 22:49:01 +0530

    Backend: Completed vulnerability scan flow with DB conflict handling and email alert integration

--- server/server.js
+++ server/server.js
@@ -13,21 +13,27 @@ app.use(express.json());
 // Test route to check if server runs
 app.get("/", (req, res) => res.send("Server is running!"));
 
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", time: new Date() });
+}); //checking
+
 // Routes – wrap in try/catch to catch missing files
 try {
   const authRoutes = require("./routes/authRoutes");
   const vulnerabilityRoutes = require("./routes/vulnerabilityRoutes");
   const projectRoutes = require("./routes/projectRoutes");
+  const alertRoute = require("./routes/alertRoute");
 
   app.use("/api/auth", authRoutes);
   app.use("/api/vulnerability", vulnerabilityRoutes);
   app.use("/api/projects", projectRoutes);
+  app.use("/api/vulnerability/alert", alertRoute); 
 } catch (err) {
   console.error("Error loading routes:", err);
 }
 
 // Get port from .env or fallback
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5004;
 
 // Start server with error handling
 app.listen(PORT, (err) => {
